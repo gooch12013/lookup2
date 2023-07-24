e1885f367f1f6fa9cd60682254097f825ab4ee34
David <gooch12013@gmail.com>
2023-07-24 14:01:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public') ));
+
+
+app.use((req, res)=>{
+
+  res.status(404);
+  res.send('<h1>Error 404:  Resource not found</h1>')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
